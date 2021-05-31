16b4818d06b7b1e72bb07009d696a8cef6cb45a0
GhofranAljabr <>
2021-05-31 00:09:43 +0000

    lab5

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-VAR person = PROMPT('ENTER YOUR NAME')
+if (condition) {
+  // block of code to be executed if the condition is true
+}
+
+var Artist = promopt('how many favorite Artist do you like');
+var Artist = '';
+
+
+if (Artist > 3) { Artist = 3;
+}
+for (var i = 0; i < Artist;i++) { Artist = 0;
+Artist += document.write('https://imgy.pro/veto/475x296/3997444.jpg'); }
